f4ecf7ca6b7d8c7e1d6967d409be4045a634efe2
Alexander Klimetschek <aklimets@adobe.com>
2015-05-22 18:21:01 -0700

    jcr access logging

--- oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/PermissionProviderImpl.java
+++ oak-core/src/main/java/org/apache/jackrabbit/oak/security/authorization/permission/PermissionProviderImpl.java
@@ -17,7 +17,9 @@
 package org.apache.jackrabbit.oak.security.authorization.permission;
 
 import java.security.Principal;
+import java.util.Collection;
 import java.util.Collections;
+import java.util.Iterator;
 import java.util.Set;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
@@ -26,9 +28,10 @@ import org.apache.jackrabbit.JcrConstants;
 import org.apache.jackrabbit.oak.api.PropertyState;
 import org.apache.jackrabbit.oak.api.Root;
 import org.apache.jackrabbit.oak.api.Tree;
+import org.apache.jackrabbit.oak.commons.PathUtils;
 import org.apache.jackrabbit.oak.plugins.tree.RootFactory;
-import org.apache.jackrabbit.oak.plugins.tree.impl.ImmutableTree;
 import org.apache.jackrabbit.oak.plugins.tree.TreeLocation;
+import org.apache.jackrabbit.oak.plugins.tree.impl.ImmutableTree;
 import org.apache.jackrabbit.oak.plugins.version.VersionConstants;
 import org.apache.jackrabbit.oak.spi.security.authorization.AuthorizationConfiguration;
 import org.apache.jackrabbit.oak.spi.security.authorization.accesscontrol.AccessControlConstants;
@@ -41,8 +44,15 @@ import org.apache.jackrabbit.oak.spi.security.authorization.permission.TreePermi
 import org.apache.jackrabbit.oak.spi.security.principal.AdminPrincipal;
 import org.apache.jackrabbit.oak.spi.security.principal.SystemPrincipal;
 import org.apache.jackrabbit.oak.spi.security.privilege.PrivilegeBits;
+import org.apache.jackrabbit.oak.spi.state.NodeState;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class PermissionProviderImpl implements PermissionProvider, AccessControlConstants, PermissionConstants, AggregatedPermissionProvider {
+
+    private final Logger log = LoggerFactory.getLogger("oak.security.access");
+
+    private String user;
 
     private final Root root;
 
@@ -67,6 +77,20 @@ public class PermissionProviderImpl implements PermissionProvider, AccessControl
         } else {
             compiledPermissions = CompiledPermissionImpl.create(immutableRoot, workspaceName, principals, acConfig);
         }
+
+        try {
+            user = root.getContentSession().getAuthInfo().getUserID();
+        } catch (Exception e) {
+            StringBuilder builder = new StringBuilder();
+            final Iterator<Principal> principalsIter = principals.iterator();
+            while (principalsIter.hasNext()) {
+                builder.append(principalsIter.next().getName());
+                if (principalsIter.hasNext()) {
+                    builder.append(",");
+                }
+            }
+            user = builder.toString();
+        }
     }
 
     @Override
@@ -78,29 +102,49 @@ public class PermissionProviderImpl implements PermissionProvider, AccessControl
     @Nonnull
     @Override
     public Set<String> getPrivileges(@Nullable Tree tree) {
-        return compiledPermissions.getPrivileges(getImmutableTree(tree));
+        final Set<String> privileges = compiledPermissions.getPrivileges(getImmutableTree(tree));
+        if (log.isTraceEnabled()) {
+            logAccess(tree == null ? "<null>" : tree.getPath(), AccessResult.OTHER, "getting privileges: " + join(toArray(privileges), ","));
+        }
+        return privileges;
     }
 
     @Override
     public boolean hasPrivileges(@Nullable Tree tree, @Nonnull String... privilegeNames) {
-        return compiledPermissions.hasPrivileges(getImmutableTree(tree), privilegeNames);
+        final boolean isGranted = compiledPermissions.hasPrivileges(getImmutableTree(tree), privilegeNames);
+        if (log.isTraceEnabled()) {
+            logAccess(tree == null ? "<null>" : tree.getPath(), isGranted, privilegeNames);
+        }
+        return isGranted;
     }
 
     @Nonnull
     @Override
     public RepositoryPermission getRepositoryPermission() {
+        if (log.isTraceEnabled()) {
+            logAccess("", AccessResult.OTHER, "<repository permissions>");
+        }
         return compiledPermissions.getRepositoryPermission();
     }
 
     @Nonnull
     @Override
     public TreePermission getTreePermission(@Nonnull Tree tree, @Nonnull TreePermission parentPermission) {
-        return compiledPermissions.getTreePermission(getImmutableTree(tree), parentPermission);
+        final TreePermission treePermission = compiledPermissions.getTreePermission(getImmutableTree(tree), parentPermission);
+        if (log.isTraceEnabled()) {
+            return new LoggingTreePermission(treePermission, tree.getPath());
+        }
+        return treePermission;
     }
 
     @Override
     public boolean isGranted(@Nonnull Tree tree, @Nullable PropertyState property, long permissions) {
-        return compiledPermissions.isGranted(getImmutableTree(tree), property, permissions);
+        final boolean isGranted = compiledPermissions.isGranted(getImmutableTree(tree), property, permissions);
+        if (log.isTraceEnabled()) {
+            String path = property == null ? tree.getPath() : PathUtils.concat(tree.getPath(), property.getName());
+            logAccess(path, isGranted, getPermissionNames(permissions));
+        }
+        return isGranted;
     }
 
     @Override
@@ -116,6 +160,10 @@ public class PermissionProviderImpl implements PermissionProvider, AccessControl
             isGranted = isGranted(tree, property, permissions);
         } else if (!isVersionStorePath(oakPath)) {
             isGranted = compiledPermissions.isGranted(oakPath, permissions);
+        }
+
+        if (log.isTraceEnabled()) {
+            logAccess(oakPath, isGranted, jcrActions);
         }
         return isGranted;
     }
@@ -147,6 +195,131 @@ public class PermissionProviderImpl implements PermissionProvider, AccessControl
     }
 
     //--------------------------------------------------------------------------
+
+    private enum AccessResult {
+        ALLOWED("ALLOWED"),
+        DENIED ("DENIED "),
+        OTHER  ("OTHER  ");
+
+        private final String text;
+
+        AccessResult(String text) {
+            this.text = text;
+        }
+
+        @Override
+        public String toString() {
+            return text;
+        }
+    }
+
+    private void logAccess(String path, boolean isGranted, String... permissions) {
+        logAccess(path, isGranted ? AccessResult.ALLOWED : AccessResult.DENIED, permissions);
+    }
+
+    private void logAccess(String path, AccessResult isGranted, String... permissions) {
+        log.trace(
+            "[{}] {} {} [{}]",
+            user,
+            isGranted.toString(),
+            path,
+            join(permissions, ",")
+        );
+    }
+
+    private String join(String[] array, String delimiter) {
+        if (array == null) {
+            return "";
+        }
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < array.length; i++) {
+            builder.append(array[i]);
+            if (i < array.length - 1) {
+                builder.append(delimiter);
+            }
+        }
+        return builder.toString();
+    }
+
+    private String[] toArray(Collection<String> strings) {
+        return strings.toArray(new String[strings.size()]);
+    }
+
+    private String[] getPermissionNames(long permissions) {
+        return toArray(Permissions.getNames(permissions));
+    }
+
+    private class LoggingTreePermission implements TreePermission {
+
+        private TreePermission original;
+
+        private String path;
+
+        public LoggingTreePermission(TreePermission original, String path) {
+            this.original = original;
+            this.path = path;
+        }
+
+        @Nonnull
+        @Override
+        public TreePermission getChildPermission(@Nonnull String childName, @Nonnull NodeState childState) {
+            return new LoggingTreePermission(original.getChildPermission(childName, childState), PathUtils.concat(path, childName));
+        }
+
+        @Override
+        public boolean canRead() {
+            final boolean canRead = original.canRead();
+            if (log.isTraceEnabled()) {
+                logAccess(path, canRead, "read");
+            }
+            return canRead;
+        }
+
+        @Override
+        public boolean canRead(@Nonnull PropertyState property) {
+            final boolean canRead = original.canRead(property);
+            if (log.isTraceEnabled()) {
+                logAccess(PathUtils.concat(path, property.getName()), canRead, "read property");
+            }
+            return canRead;
+        }
+
+        @Override
+        public boolean canReadAll() {
+            final boolean canReadAll = original.canReadAll();
+            if (log.isTraceEnabled()) {
+                logAccess(path, canReadAll, "read all");
+            }
+            return canReadAll;
+        }
+
+        @Override
+        public boolean canReadProperties() {
+            final boolean canReadProperties = original.canReadProperties();
+            if (log.isTraceEnabled()) {
+                logAccess(path, canReadProperties, "read properties");
+            }
+            return canReadProperties;
+        }
+
+        @Override
+        public boolean isGranted(long permissions) {
+            final boolean isGranted = original.isGranted(permissions);
+            if (log.isTraceEnabled()) {
+                logAccess(path, isGranted, getPermissionNames(permissions));
+            }
+            return isGranted;
+        }
+
+        @Override
+        public boolean isGranted(long permissions, @Nonnull PropertyState property) {
+            final boolean isGranted = original.isGranted(permissions, property);
+            if (log.isTraceEnabled()) {
+                logAccess(PathUtils.concat(path, property.getName()), isGranted, getPermissionNames(permissions));
+            }
+            return isGranted;
+        }
+    }
 
     private boolean isAdmin(Set<Principal> principals) {
         Set<String> adminNames = acConfig.getParameters().getConfigValue(PARAM_ADMINISTRATIVE_PRINCIPALS, Collections.EMPTY_SET);
